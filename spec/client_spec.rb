38574b658ffb786ae1bb33888b01c8f480e3988f
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2016-12-09 10:03:44 -0800

    pass rspec for client name method

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -11,4 +11,17 @@ describe(Client) do
       expect(Client.all).to(eq([]))
     end
   end
+
+  describe('#name') do
+    it('returns client name') do
+      expect(@client.name).to(eq("Abigail"))
+    end
+  end
+  #
+  # describe('#save') do
+  #   it('returns saved content from the database') do
+  #     @client.save
+  #     expect(Client.all).to(eq([@client]))
+  #   end
+  # end
 end
