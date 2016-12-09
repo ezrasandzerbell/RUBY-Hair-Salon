d85e62b8ce236f4eba8fbf9c6775965172bc91eb
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2016-12-09 10:50:22 -0800

    add passing rspecs for stylist class methods including name, id, all, save, and ==

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,6 +1,6 @@
 class Stylist
 
-  attr_reader(:idm, :name)
+  attr_reader(:id, :name)
 
   define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
---
+++ spec/stylist_spec.rb
@@ -0,0 +1,44 @@
+require('spec_helper')
+
+describe(Stylist) do
+
+  before() do
+    @stylist = Stylist.new(:id => nil, :name => "Edward Scissorhands")
+  end
+
+  describe('#all') do
+    it('returns an empty array for now') do
+      expect(Stylist.all).to(eq([]))
+    end
+  end
+
+  describe('#name') do
+    it('returns stylist name') do
+      expect(@stylist.name).to(eq("Edward Scissorhands"))
+    end
+  end
+
+  describe('#stylist_id') do
+    it('returns id of stylist') do
+      @stylist.save
+      expect(@stylist.id).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#==') do
+    it('tests for equivalence, omiting Class instance id') do
+      stylist1 = Stylist.new(:id => 1, :name => "biscuit")
+      stylist2 = Stylist.new(:id => 1, :name => "biscuit")
+      expect(stylist1 == stylist2).to(eq(true))
+    end
+  end
+
+  describe('#save') do
+    it('returns saved content from the database') do
+      stylist1 = Stylist.new(:id => 1, :name => "biscuit")
+      stylist2 = Stylist.new(:id => 2, :name => "Charlie")
+      stylist2.save
+      expect(Stylist.all).==([stylist2])
+    end
+  end
+end
